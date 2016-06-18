3eb842dbeb7053c8dd416abade4ac64aacb7100a
wendycwong <wendycwong@yahoo.com>
2016-06-18 11:08:44 -0700

    PUBDEV-1612: Orc parser.  Orc parser is built like th Avro parser built by Michal M.  The two parsers are built using the current parser infrastructure built by Tomas N.  Due to the strange configuration of the Orc file with information needed to parse the whole file found at the end of the file instead of the beginning, Tomas has changed the parser framework to accomodate this.  In addition, orc file parsing does not use the normal chunking method either.  Orc parser chunking is done per stripe as designed by Tomas.  We currently do not support multi-file parsing, parsing use user metadata.
    Big update of ORC parsing from Tomas N:
    
    In order to make ORC parse work distributed and for hdfs I made following changes:
    
    1. guessSetup takes the whole file - it is needed to read metadata stored at the end of file.
    2.  The file is used as is, from Hdfs or local drive/nfs (no more copying the data to local disk)
    3. input file is chunked so that the number of chunks matches exactly number of stripes in the orc file - then we can easily parse the data using ORC libraries from hadoop (each parse task simply parses it's own stripe - no need to read data ourselves or manipulates the raw bytes directly.
    4. When specifying columns we want to read ,w e need to flatten non-primitive data types (orc expects them to be flattened in pre-order tree walk).

--- h2o-parsers/h2o-orc-parser/src/main/java/water/parser/orc/OrcParserProvider.java
+++ h2o-parsers/h2o-orc-parser/src/main/java/water/parser/orc/OrcParserProvider.java
@@ -26,7 +26,7 @@ import static water.fvec.FileVec.getPathForKey;
 public class OrcParserProvider extends ParserProvider {
 
   /* Setup for this parser */
-  static ParserInfo ORC_INFO = new ParserInfo("ORC", DefaultParserProviders.MAX_CORE_PRIO + 20, true, false);
+  static ParserInfo ORC_INFO = new ParserInfo("ORC", DefaultParserProviders.MAX_CORE_PRIO + 20, true);
 
   @Override
   public ParserInfo info() {
@@ -53,7 +53,7 @@ public class OrcParserProvider extends ParserProvider {
    * @param inputs  input keys
    * @param requiredSetup  user given parser setup
    * @return
-     */
+   */
   @Override
   public ParseSetup createParserSetup(Key[] inputs, ParseSetup requiredSetup) {
 
@@ -85,8 +85,8 @@ public class OrcParserProvider extends ParserProvider {
    * @param f
    * @param columnNames
    * @param columnTypes
-     * @return
-     */
+   * @return
+   */
   public ParseSetup readSetup(FileVec f, String[] columnNames, byte[] columnTypes) {
     try {
       Reader orcFileReader = getReader(f);
@@ -119,10 +119,8 @@ public class OrcParserProvider extends ParserProvider {
         f.setChunkSize(stp._chunk_size = (int)f.length());
         return stp;
       }
-      stp._chunk_size = (int)(f.length()/(stripesInfo.size()));
-      if((f.length()%stripesInfo.size()) != 0) // need  exact match between stripes and chunks
-        stp._chunk_size = (int)((f.length()+stripesInfo.size())/stripesInfo.size());
-      f.setChunkSize(stp._chunk_size);
+      f.setNChunks(stripesInfo.size());
+      stp._chunk_size = f._chunkSize;
       assert f.nChunks() == stripesInfo.size(); // ORC parser needs one-to one mapping between chunk and strip (just ids, offsets do not matter)
       return stp;
     } catch(IOException ioe) {
@@ -134,8 +132,8 @@ public class OrcParserProvider extends ParserProvider {
   public ParseSetup setupLocal(Vec v, ParseSetup setup){
     if(!(v instanceof FileVec)) throw H2O.unimpl("ORC only implemented for HDFS / NFS files");
     try {
-
       ((OrcParser.OrcParseSetup)setup).setOrcFileReader(getReader((FileVec)v));
+
       return setup;
 
     } catch (IOException e) {throw new RuntimeException(e);}
