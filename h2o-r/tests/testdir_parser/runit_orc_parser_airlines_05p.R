8a51d870784b29522bdd0d696ebaef4409292a06
wendycwong <wendycwong@yahoo.com>
2016-07-25 15:42:18 -0700

    PUBDEV-1612: orc parser.  Finished off user specified column types and added runit and pyunit tests.

---
+++ h2o-r/tests/testdir_parser/runit_orc_parser_airlines_05p.R
@@ -0,0 +1,24 @@
+setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
+source("../../scripts/h2o-r-test-setup.R")
+################################################################################
+##
+## Verify that we can specify data column type in R as well
+##
+################################################################################
+
+
+test.continuous.or.categorical <- function() {
+
+  airline_part0_csv <- h2o.importFile(locate("bigdata/laptop/parser/orc/000000_0.csv"))
+
+  col.names = c("_col8", "_col16", "_col17", "_col22", "_col29", "_col30")
+  col.types = c("enum", "enum", "enum", "enum", "enum", "enum")
+
+  airline_part0_orc <- h2o.importFile(locate("bigdata/laptop/parser/orc/airlines_all.05p/000000_0"),
+  col.types=list(by.col.name=col.names, types=col.types))
+
+  h2o_and_h2o_equal(airline_part0_csv, airline_part0_orc)   # compare two frames and make sure they are equal
+}
+
+doTest("Veryfying R Orc Parser Can Declare Types on Import", test.continuous.or.categorical)
+
