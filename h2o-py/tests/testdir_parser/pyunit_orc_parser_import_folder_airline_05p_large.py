e6998d9fc87dfcb3d5488ffb227e6d6051d80615
wendycwong <wendycwong@yahoo.com>
2016-07-28 16:18:56 -0700

    PUBDEV-1612: orc parser.  Added multi-file tests for Nidhi to review.  Not for prime time yet.

---
+++ h2o-py/tests/testdir_parser/pyunit_orc_parser_import_folder_airline_05p_large.py
@@ -0,0 +1,33 @@
+from __future__ import print_function
+import sys
+sys.path.insert(1,"../../")
+import h2o
+from tests import pyunit_utils
+
+
+def import_folder():
+    """
+    This test will build a H2O frame from importing the bigdata/laptop/parser/orc/airlines_05p_orc_csv
+    from and build another H2O frame from the multi-file orc parser using multiple orc files that are
+    saved in the directory bigdata/laptop/parser/orc/airlines_05p_orc.  It will compare the two frames
+    to make sure they are equal.
+    :return: None if passed.  Otherwise, an exception will be thrown.
+    """
+
+    tol_time = 200              # comparing in ms or ns
+    tol_numeric = 1e-5          # tolerance for comparing other numeric fields
+    numElements2Compare = 10   # choose number of elements per column to compare.  Save test time.
+
+#    multi_file_csv = h2o.import_file(path=pyunit_utils.locate("bigdata/laptop/parser/orc/airlines_05p_orc_csv"))
+#    multi_file_csv = h2o.import_file(path=pyunit_utils.locate("bigdata/laptop/airlines_all.05p.csv"))
+#    multi_file_orc = h2o.import_file(path=pyunit_utils.locate("bigdata/laptop/parser/orc/airlines_all.05p"))
+    multi_file_orc = h2o.import_file(path=pyunit_utils.locate("bigdata/laptop/parser/orc/airlines_05p_orc"))
+    # make sure orc multi-file and single big file create same H2O frame
+    # assert pyunit_utils.compare_frames(multi_file_orc , multi_file_csv, numElements2Compare, tol_time, tol_numeric), \
+    #     "H2O frame parsed from multiple orc and single csv files are different!"
+
+
+if __name__ == "__main__":
+    pyunit_utils.standalone_test(import_folder)
+else:
+    import_folder()
