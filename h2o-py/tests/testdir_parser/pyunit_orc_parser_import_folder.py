3312a92a371204078c765dbb7bcaec66674a9059
wendycwong <wendycwong@yahoo.com>
2016-07-29 09:41:21 -0700

    PUBDEV-1612: orc parser.  Fixed bug in test.

--- h2o-py/tests/testdir_parser/pyunit_orc_parser_import_folder.py
+++ h2o-py/tests/testdir_parser/pyunit_orc_parser_import_folder.py
@@ -12,23 +12,11 @@ def import_folder():
   numElements2Compare = 100   # choose number of elements per column to compare.  Save test time.
 
   multi_file_csv = h2o.import_file(path=pyunit_utils.locate("smalldata/synthetic_perfect_separation"))
-  combined_csv = h2o.import_file(path=pyunit_utils.locate("smalldata/parser/orc/orc2csv/combined.csv"))
-
-  # make sure csv multi-file and single big file create same H2O frame
-  assert pyunit_utils.compare_frames(multi_file_csv, combined_csv, numElements2Compare, tol_time, tol_numeric, True), \
-    "H2O frame parsed from multiple and single csv files are different!"
-
   multi_file_orc = h2o.import_file(path=pyunit_utils.locate("smalldata/parser/orc/synthetic_perfect_separation"))
-  combined_orc = h2o.import_file(path=pyunit_utils.locate("smalldata/parser/orc/combined.orc"))
 
   # make sure orc multi-file and single big file create same H2O frame
-  assert pyunit_utils.compare_frames(multi_file_orc , combined_orc, numElements2Compare, tol_time, tol_numeric, True), \
+  assert pyunit_utils.compare_frames(multi_file_orc , multi_file_csv, numElements2Compare, tol_time, tol_numeric, True), \
     "H2O frame parsed from multiple orc and single orc files are different!"
-
-
-  # make sure H2O frame from csv and orc agrees.
-  assert pyunit_utils.compare_frames(combined_csv, combined_orc, numElements2Compare, tol_time, tol_numeric, True), \
-    "H2O frame parsed from orc and csv files are different!"
 
 
 if __name__ == "__main__":
