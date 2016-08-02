17d2568ef2eab4a85d47a7a801bc197e626de3f6
wendycwong <wendycwong@yahoo.com>
2016-08-01 19:51:07 -0700

    PUBDEV-1612: orc parser.  Need to turn on warning option in runit_orc_parser_baddata.R

--- h2o-r/tests/testdir_parser/runit_orc_parser_baddata.R
+++ h2o-r/tests/testdir_parser/runit_orc_parser_baddata.R
@@ -2,23 +2,17 @@ setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
 source("../../scripts/h2o-r-test-setup.R")
 
 # This test is written to make sure that warnings from Orc Parser are passed to the R client.
-# In particulare, the first two Orc files contain unsupported column types.
+# In particular, the first two Orc files contain unsupported column types.
 # The third Orc file contains big integer values that are used by sentinel for H2O frame.
 
 test.orc_parser.bad_data <- function() {
+  options(warn=1)
   # These files contain unsupported data types
-  frame1 = h2o.importFile(locate("smalldata/parser/orc/TestOrcFile.testStringAndBinaryStatistics.orc"))
-#  Sys.sleep(2)
-#  expect_warning(h2o.importFile(locate("smalldata/parser/orc/TestOrcFile.testStringAndBinaryStatistics.orc")))
+  expect_warning(h2o.importFile(locate("smalldata/parser/orc/TestOrcFile.testStringAndBinaryStatistics.orc")))
+  expect_warning(h2o.importFile(locate("smalldata/parser/orc/TestOrcFile.emptyFile.orc")))
 
-  frame2 = h2o.importFile(locate("smalldata/parser/orc/TestOrcFile.emptyFile.orc"))
-#  Sys.sleep(2)
-#  expect_warning(h2o.importFile(locate("smalldata/parser/orc/TestOrcFile.emptyFile.orc")))
-
-  frame3 = h2o.importFile(locate("smalldata/parser/orc/nulls-at-end-snappy.orc"))
-#  Sys.sleep(2)
 #  # This file contains big integer value Long.MIN_VALUE that is used for sentinel
-#  expect_warning(h2o.importFile(locate("smalldata/parser/orc/nulls-at-end-snappy.orc")))
+  expect_warning(h2o.importFile(locate("smalldata/parser/orc/nulls-at-end-snappy.orc")))
 
 }
 
