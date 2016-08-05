d60101335f228e5f9f58810169f0c957b71c49c4
wendycwong <wendycwong@yahoo.com>
2016-08-05 10:38:09 -0700

    PUBDEV-1612: orc parser.  Try to fixed problem with Jenkins test failing baddata and not passing warning to user.

--- h2o-r/tests/testdir_parser/runit_orc_parser_baddata.R
+++ h2o-r/tests/testdir_parser/runit_orc_parser_baddata.R
@@ -10,17 +10,19 @@ test.orc_parser.bad_data <- function() {
   
   # These files contain unsupported data types
   frame = h2o.importFile(locate("smalldata/parser/orc/TestOrcFile.testStringAndBinaryStatistics.orc"))
+  expect_warning(h2o.importFile(locate("smalldata/parser/orc/TestOrcFile.testStringAndBinaryStatistics.orc")))
   frame = h2o.importFile(locate("smalldata/parser/orc/TestOrcFile.emptyFile.orc"))
-  
+  expect_warning(h2o.importFile(locate("smalldata/parser/orc/TestOrcFile.emptyFile.orc")))
   # This file contains big integer value Long.MIN_VALUE that is used for sentinel
   frame = h2o.importFile(locate("smalldata/parser/orc/nulls-at-end-snappy.orc"))
+  expect_warning(h2o.importFile(locate("smalldata/parser/orc/nulls-at-end-snappy.orc")))
   
-  b = warnings()    # collect all warnings into a list
-  
-  if (length(b) < 7) {
-    browser()
-    throw("Not all warning messages are passed from Java to R client.")
-  }
+#   b = warnings()    # collect all warnings into a list
+#   print(length(b))
+#   if (length(b) < 1) {
+#      browser()
+# #     throw("Not all warning messages are passed from Java to R client.")
+#    }
 }
 
 doTest("Orc Parser: make sure warnings are passed to user.", test.orc_parser.bad_data)
