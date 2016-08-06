d888e17049ef70072f65620251aa7bb3dac07c7f
wendycwong <wendycwong@yahoo.com>
2016-08-05 17:05:17 -0700

    PUBDEV-1612: orc parser.  Tried to make it run on windows again by changing the file path.

--- h2o-core/src/main/java/water/fvec/FileVec.java
+++ h2o-core/src/main/java/water/fvec/FileVec.java
@@ -13,8 +13,17 @@ public abstract class FileVec extends ByteVec {
   public static String getPathForKey(Key k) {
     final int off = k._kb[0]==Key.CHK   || k._kb[0]==Key.VEC ? Vec.KEY_PREFIX_LEN : 0;
     String p = new String(k._kb,off,k._kb.length-off);
-    if(p.startsWith("nfs:"))
+
+    Log.info("File with path is "+p);
+
+
+    if(p.startsWith("nfs:/"))
       p = p.substring("nfs:/".length());
+    else if (p.startsWith("nfs:\\"))
+      p = p.substring("nfs:\\".length());
+
+    Log.info("File after change "+p);
+
     return p;
   }
   /** Log-2 of Chunk size. */
