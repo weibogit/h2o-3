8d0a4f87ae2adcfa949cd82a7b36fd279ede42a6
Nidhi Mehta <nidhimehta@Nbook.local>
2016-08-08 18:59:17 -0600

    orc multifile runit PUBDEV-3200

---
+++ h2o-r/tests/testdir_parser/runit_NOPASS_orc_parser_airlines_05p_large.R
@@ -0,0 +1,28 @@
+setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
+source("../../scripts/h2o-r-test-setup.R")
+################################################################################
+##
+## This tests Orc multifile parser by comparing the summary of the original csv frame with the h2o parsed orc frame
+##
+################################################################################
+
+
+test.continuous.or.categorical <- function() {
+
+
+	original = h2o.importFile(locate("bigdata/laptop/parser/orc/Datafor_pubdev3200/airlines_all.05p.csv"),destination_frame = "original",
+                     col.types=c("Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Enum","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Enum","Enum","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Enum","Enum"))
+	csv = h2o.importFile(locate("bigdata/laptop/parser/orc/Datafor_pubdev3200/air05_csv"),destination_frame = "csv",col.names = names(original),
+                     col.types=c("Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Enum","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Enum","Enum","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Enum","Enum"))
+	orc = h2o.importFile(locate("bigdata/laptop/parser/orc/Datafor_pubdev3200/air05_orc"),destination_frame = "orc",col.names = names(original),
+                     col.types=c("Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Enum","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Enum","Enum","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Numeric","Enum","Enum"))
+
+  	expect_equal(summary(csv),summary(original))
+  	
+  	for(i in 1:ncol(csv)){
+       print(i)
+       expect_equal(summary(csv[,i]),summary(original[,i]))
+    }
+}
+
+doTest("Test orc multifile parser", test.continuous.or.categorical)
